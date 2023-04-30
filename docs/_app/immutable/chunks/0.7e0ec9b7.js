057ab30285b4e0ce3b29a011e1415375a6a22d8c
Ilya <enotn36@gmail.com>
2023-04-30 16:42:54 +0300

    fix: fix bugs

---
+++ docs/_app/immutable/chunks/0.7e0ec9b7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_layout.svelte.5c6b5d5f.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.7aa87f16.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.6eb40c17.js";export{t as component};
