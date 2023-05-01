3767404dba4445d10d9148cd808648d37642548e
Ilya <enotn36@gmail.com>
2023-05-01 18:21:45 +0300

    fix: fix bugs

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-    build: {
-        outDir: './docs', // relative to index.html
-        // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
-    }
 });
